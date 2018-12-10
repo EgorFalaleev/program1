06c979f66ef01ec15ebc9d8bd85f3cd7f770be16
Egor <Egor1268@gmail.com>
2018-12-10 20:49:16 +0300

    Made Quadratic Equation using pass by reference

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void Quadratic_Equation();
+void Quadratic_Equation(float  &x, float &y, float &z);
 
 int main()
 {
-	float num1,num2;
+	
+	float a,b,c, num1,num2;
 	int operation;
 
 	cout << "Choose operation:" << endl;
@@ -38,7 +39,14 @@ int main()
 	}
 	else if (operation ==5)
 	{
-		Quadratic_Equation();
+		cout << "Write first number (a): ";
+		cin >> a;	
+		cout << "Write second number (b): ";
+		cin >> b;
+		cout << "Write third number (c): ";
+		cin >> c;
+
+		Quadratic_Equation(a,b,c);
 	}
 	else
 	{
@@ -48,32 +56,23 @@ int main()
 	return 0;
 }
 
-void Quadratic_Equation()
+void Quadratic_Equation(float  &x, float &y, float &z)
 {
-	float a,  b,  c;
 	float d,  x1,  x2;
-	cout << "Write first number (a): " << endl;
-	cin >> a;
-	cout << "Write second number(b): " << endl;
-	cin >> b;
-	cout << "Write third number(c): " << endl;
-	cin >> c;
 
-	cout << a << "x^2 + " << b << "x + " << c << "= 0" << endl;
-
-	d = b * b - 4 * a * c;
+	d = y * y - 4 * x * z;
 	cout << "D = " << d << endl;
 
 	if (d > 0)
 	{
-		x1 = (-b + sqrt(d)) / (2 * a);
-		x2 = (-b - sqrt(d)) / (2 * a);
+		x1 = (-y + sqrt(d)) / (2 * x);
+		x2 = (-y - sqrt(d)) / (2 * x);
 
 		cout << "x1 = " << x1 << "; x2 = " << x2 << endl;
 	}
 	else if (d == 0)
 	{
-		x1 = (-b + sqrt(d)) / (2 * a);
+		x1 = (-y + sqrt(d)) / (2 * x);
 		cout << "x1 = " << x1 << endl;
 	}
 	else
