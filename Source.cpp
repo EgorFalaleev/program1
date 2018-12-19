77492f6d132964173cd8ea92e0056ffb2f847d6c
Egor <Egor1268@gmail.com>
2018-12-19 20:43:26 +0300

    added angles and implementation

--- Source.cpp
+++ Source.cpp
@@ -3,22 +3,30 @@ using namespace std;
 
 class Figure
 {
-public:
-	virtual double get_Area() = 0;
-	virtual double get_Perimeter() = 0;
 protected:
 	double height;
 	double length;
+public:
+	virtual double get_Area() = 0;
+	virtual double get_Perimeter() = 0;
 };
 
-class Rectangle : public Figure
+class Angles
+{
+protected:
+	int number;
+public:
+	virtual void count_Angles() { cout << "Figure has " << number << "angles"; }
+};
+
+class Rectangle : public Figure, public Angles
 {
 public:
 	double get_Area() { return height * length; }
 	double get_Perimeter() { return (height + length) * 2; }
 };
 
-class RightTriangle : public Figure
+class RightTriangle : public Figure, public Angles
 {
 private:
 	double hypotenuse = sqrt(height*height + length * length);
@@ -36,8 +44,20 @@ public:
 	double get_Perimeter() { return 2 * 3.14*radius; }
 };
 
+
 int main()
 {
-	Rectangle* rect;
-	rect->get_Area();
+	Rectangle rect;
+	rect.get_Area();
+	rect.get_Perimeter();
+	rect.count_Angles();
+
+	RightTriangle trgl;
+	trgl.get_Area();
+	trgl.get_Perimeter();
+	trgl.count_Angles();
+
+	Circle cir;
+	cir.get_Area();
+	cir.get_Perimeter;
 }
