2a0c336e43ef5dc0b803339fadb52a721665aec5
Egor <Egor1268@gmail.com>
2018-12-19 16:44:47 +0300

    coded classes

--- Source.cpp
+++ Source.cpp
@@ -1,86 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void Quadratic_Equation(float x, float y, float z, float &x1, float &x2);
+class Figure
+{
+public:
+	virtual double get_Area() = 0;
+	virtual double get_Perimeter() = 0;
+protected:
+	double height;
+	double length;
+};
+
+class Rectangle : public Figure
+{
+public:
+	double get_Area() { return height * length; }
+	double get_Perimeter() { return (height + length) * 2; }
+};
+
+class RightTriangle : public Figure
+{
+private:
+	double hypotenuse = sqrt(height*height + length * length);
+public:
+	double get_Area() { return height * length / 2; }
+	double get_Perimeter() { return height + length + hypotenuse; }
+};
+
+class Circle : public Figure
+{
+private:
+	double radius;
+public: 
+	double get_Area() { return radius * radius*3.14; }
+	double get_Perimeter() { return 2 * 3.14*radius; }
+};
 
 int main()
 {
-	
-	float a,b,c, num1,num2;
-	int operation;
-
-	cout << "Choose operation:" << endl;
-	cout << "1. Addiction" << '\n' << "2. Difference" << '\n' << "3. Multiplication" << '\n' << "4. Division" << '\n' << "5. Quadratic Equation" << endl;
-	cin >> operation;
-
-	if (operation <= 4 && operation>0)
-	{
-		cout << "Write first number: ";
-		cin >> num1;
-		cout << "Write second number: ";
-		cin >> num2;
-	
-		switch (operation)
-		{
-		case 1:
-			cout << "The result is " << num1 + num2 << endl;
-			break;
-		case 2:
-			cout << "The result is " << num1 - num2 << endl;
-			break;
-		case 3:
-			cout << "The result is " << num1 * num2 << endl;
-			break;
-		case 4:
-			cout << "The result is " << num1 / num2 << endl;
-			break;
-		}
-
-	}
-	else if (operation ==5)
-	{
-		cout << "Write first number (a): ";
-		cin >> a;	
-		cout << "Write second number (b): ";
-		cin >> b;
-		cout << "Write third number (c): ";
-		cin >> c;
-
-		float y1, y2;
-
-		Quadratic_Equation(a,b,c, y1, y2);
-		cout << "x1 = " << y1 << "; x2 = " << y2 << endl;
-
-	}
-	else
-	{
-		cout <<  "Something goes wrong. Make sure you" << '\'' << "ve choosed the right operation.";
-	}
-
-	return 0;
+	Rectangle* rect;
+	rect->get_Area();
 }
-
-void Quadratic_Equation(float  x, float y, float z,float &x1,float &x2)
-{
-	float d;
-
-	d = y * y - 4 * x * z;
-
-	if (d > 0)
-	{
-		x1 = (-y + sqrt(d)) / (2 * x);
-		x2 = (-y - sqrt(d)) / (2 * x);
-
-	}
-	else if (d == 0)
-	{
-		x1 = (-y + sqrt(d)) / (2 * x);
-		x2 = (-y + sqrt(d)) / (2 * x);
-
-	}
-	else
-	{
-	}
-}
-
-
