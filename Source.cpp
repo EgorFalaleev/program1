db87e38e623a478527386eb0a7d0bb9b8197fa6a
Egor <Egor1268@gmail.com>
2018-12-21 10:45:27 +0300

    constructors added

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,6 @@ class Figure
 public:
 	virtual double get_Area() = 0;
 	virtual double get_Perimeter() = 0;
-	virtual void set_values(double, double) = 0;
 };
 
 class IAngleCountable
@@ -19,10 +18,10 @@ class Rectangle : public Figure, public IAngleCountable
 {
 public:
 	double height, length;
+	Rectangle(double h, double l): height(h), length(l) {}
 	virtual double get_Area() { return height * length; }
 	virtual double get_Perimeter() { return (height + length) * 2; }
 	virtual int count_Angles() { return 4; }
-	virtual void set_values(double h, double l) { height = h; length = l; }
 };
 
 class RightTriangle : public Figure, public IAngleCountable
@@ -30,43 +29,38 @@ class RightTriangle : public Figure, public IAngleCountable
 public:	
 	double get_Hypotenuse() { return sqrt(cathetus1*cathetus1 + cathetus2 * cathetus2); }
 	double cathetus1, cathetus2;
+	RightTriangle(double c1, double c2): cathetus1(c1), cathetus2(c2) {}
 	virtual double get_Area() { return cathetus1 * cathetus2 / 2; }
 	virtual double get_Perimeter() { return cathetus1 + cathetus2 + get_Hypotenuse(); }
 	virtual int count_Angles() { return 3; }
-	virtual void set_values(double k1, double k2) { cathetus1 = k1; cathetus2 = k2; }
 };
 
 class Circle : public Figure
 {
 public:
 	double radius;
+	Circle(double r): radius(r) {}
 	virtual double get_Area() { return radius * radius*3.14; }
 	virtual double get_Perimeter() { return 2 * 3.14*radius; }
-	virtual void set_values(double r, double x) { radius = r; x = 0; }
 };
 
 int main()
 {
-	Figure* rect = new Rectangle();
-	rect->set_values(3, 4);
+	Figure* rect = new Rectangle(5,5);
 	cout << "Rectangle area = " << rect->get_Area() << endl;
 	cout << "Rectangle perimeter = " << rect->get_Perimeter() << endl;
 
-	Figure* trgl = new RightTriangle();
-	trgl->set_values(2, 5);
+	Figure* trgl = new RightTriangle(10,3);
 	cout << "Triangle area = " << trgl->get_Area() << endl;
 	cout << "Triangle perimeter = " << trgl->get_Perimeter() << endl;
 
-	Figure* cir = new Circle();
-	cir->set_values(1, 0);
+	Figure* cir = new Circle(2);
 	cout << "Circle area =" << cir->get_Area() << endl;
 	cout << "Circle perimeter = " << cir->get_Perimeter() << endl;
 
-	IAngleCountable* anglesrect = new Rectangle();
+	IAngleCountable* anglesrect = new Rectangle(0,0);
 	cout << "Rectangle has " << anglesrect->count_Angles() << " angles." << endl;
 
-	IAngleCountable* anglestrgl = new RightTriangle();
+	IAngleCountable* anglestrgl = new RightTriangle(0,0);
 	cout << "Triangle has " << anglestrgl->count_Angles() << " angles." << endl;
-
-	system("pause");
 }
